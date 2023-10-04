b87a162205cd0bae8b97de8ec1b68defd5779d2a
Desmond16170 <luisfer1029@gmail.com>
2023-10-03 22:16:53 -0600

    Cambiar el nombre de la carpeta de Examen1 a Examen 1

---
+++ T1/src/main.cpp
@@ -0,0 +1,13 @@
+///librerias///
+#include <stdio.h> 
+#include <stdlib.h>
+#include <conio.h>
+#include "menu1.hpp"
+#include "menu2.hpp"
+#include "fun.cpp"
+#include <vector>
+
+/// main principal 
+int main() {/// iniciar el Menu de inicio
+    inicio(); /// Llama dentro del archivo fun.cpp a todo el programa 
+}
