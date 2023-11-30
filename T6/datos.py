a765af93ac268940eb846b2caac52095d2814123
Desmond16170 <luisfer1029@gmail.com>
2023-11-29 22:38:53 -0600

    datos.py actualiado

--- T6/datos.py
+++ T6/datos.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-import sns
+import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
